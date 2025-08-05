1c254d9d341ff8c4c569c7c9e03b9e9a8793e29a
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-08-05 15:30:06 +0900

    Update TodoController.java

--- apps/todo-list/backend/src/main/java/org/example/todov1/controller/TodoController.java
+++ apps/todo-list/backend/src/main/java/org/example/todov1/controller/TodoController.java
@@ -56,7 +56,7 @@ public class TodoController {
 
     @GetMapping("/ping")
     public String ping() {
-        return "pong - CI/CD demo v1 v2v2 v3";
+        return "pong - CI/CD demo v1 v2v2 v3v4";
     }
 }
 
